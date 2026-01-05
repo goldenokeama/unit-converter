83849c394696ff652891b6a74c5a939f4aa6cd40
Golden1636 <okeamagolden@yahoo.com>
2026-01-05 12:27:32 +0100

    got the meter/feet conversion working

--- index.js
+++ index.js
@@ -3,3 +3,25 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+const inputEl = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+
+const lengthTextParagraph = document.getElementById("length-text");
+const volumeTextParagraph = document.getElementById("volume-text");
+const massTextParagraph = document.getElementById("mass-text");
+
+convertBtn.addEventListener("click", convertUnit);
+
+function convertUnit() {
+  const inputValue = Number(inputEl.value);
+
+  const feetValue = (inputValue * 3.281).toFixed(3);
+  const gallonValue = (inputValue * 0.264).toFixed(3);
+  const poundValue = (inputValue * 2.204).toFixed(3);
+
+  const metersValue = (inputValue / 3.281).toFixed(3);
+  const litersValue = (inputValue / 0.264).toFixed(3);
+  const kilogramValue = (inputValue / 2.204).toFixed(3);
+
+  lengthTextParagraph.textContent = `${inputValue} meters = ${feetValue} feet | ${inputValue} feet = ${metersValue} meters`;
+}
